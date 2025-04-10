a0b43193983e18e7030001e3298c3f5952ba177a
Jordistt <jst33@alu.ua.es>
2025-04-10 13:28:01 +0200

    Añadido conexión a BD, implementado usuarios

--- app.py
+++ app.py
@@ -1,27 +1,59 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, request, flash, session, url_for, redirect
+from chat import iniciar_sesion, registrar_usuario, crear_bd
+
 
 app = Flask(__name__)
-
+app.secret_key = "183buc80y01c91ñ21z$%//$Cc12%"
 @app.route("/")
 def index():
+    crear_bd()
     return render_template("index.html")
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
-        usuario = request.form["usuario"]
-        contrasena = request.form["contrasena"]
-        return f'Usuario: {usuario}    Contraseña: {contrasena}'
-    return render_template("login.html")
+        nombre = request.form["usuario"]
+        contraseña = request.form["contrasena"]
+        
+        if iniciar_sesion(nombre, contraseña):  # Esta es tu función propia
+            session["usuario"] = nombre
+            return 'Implementar esta parte. Ver anuncios, crear anuncios, buscar...'  # O a donde quieras llevar al usuario
+        else:
+            flash("Usuario o contraseña incorrectos")
+            return redirect(url_for("login"))
+        
+    return render_template("login.html") 
 
 @app.route("/registro", methods=["GET", "POST"])
 def registro():
     if request.method == "POST":
-        usuario = request.form["usuario"]
+        usuario = request.form["usuario"].strip()
         contrasena = request.form["contrasena"]
         confirmar = request.form["confirmar"]
-        return f'Usuario: {usuario}    Contraseña: {contrasena}'
+        print('peticion post')
+        print(usuario, contrasena, confirmar)
+
+        # Validación de campos vacíos
+        if not usuario or not contrasena or not confirmar:
+            flash("Todos los campos son obligatorios.")
+            return redirect(url_for("registro"))
+
+        # Verificación de contraseñas
+        if contrasena != confirmar:
+            flash("Las contraseñas no coinciden.")
+            return redirect(url_for("registro"))
+
+        # Intentar registrar al usuario
+        creado = registrar_usuario(usuario, contrasena)
+        if creado:
+            flash("Registrado correctamente, por favor, inicie sesión")
+            return redirect(url_for("login"))
+        else:
+            flash("No se pudo registrar el usuario. Es posible que ya exista.")
+            return redirect(url_for("registro"))
+
     return render_template("registro.html")
+
 
 @app.route("/valoraciones")
 def valoraciones():
